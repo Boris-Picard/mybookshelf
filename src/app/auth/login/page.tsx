f27a3fb3b9de7d0ba13e5a643485bffc1e30703c
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 <picard.boris@gmail.com>
2024-07-20 12:32:42 +0200

    refactor: removed nodemailer etc

--- src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from "@/components/login/SignInForm";
-
-export default function SignIn() {
-  return (
-    <main className="flex min-h-screen items-center p-24">
-      <SignInForm />
-    </main>
-  );
-}
