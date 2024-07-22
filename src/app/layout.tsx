95ea8ce08b7a5b83303fa8530d9b6124970b8b3d
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 <picard.boris@gmail.com>
2024-07-22 16:29:16 +0200

    feat: added routes protection middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { SessionProvider } from "next-auth/react";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -19,22 +20,24 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-zinc-900 font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen bg-zinc-900 font-sans antialiased",
+            fontSans.variable
+          )}
         >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -2,42 +2,37 @@ import NextAuth from "next-auth"
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import authConfig from "./auth.config"
 
 const prisma = new PrismaClient()
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {
+  signIn,
+  signOut,
+  handlers,
+  auth,
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub, Google],
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days to session expiry
-    updateAge: 24 * 60 * 60, // 24 hours to update session data into database
-  },
-  callbacks: {
-    async session({ session, user }) {
-      session.userId = user.id;
-      return session;
-    },
-    authorized({ auth, request: { nextUrl } }) {
-      console.log(auth);
-      console.log(nextUrl);
-
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
-    },
-  },
-  // secure: process.env.AUTH_TOKEN,
+  session: { strategy: "jwt" },
   pages: {
     signIn: "/",
+    // error: "/auth/error",
   },
-})
+  callbacks: {
+    session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id;
+      }
+
+      return session;
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    }
+  },
+  ...authConfig,
+});
