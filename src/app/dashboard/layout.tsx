8204cc53ae048aa9f2a66de23493e3b7c6f5e519
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 <picard.boris@gmail.com>
2024-07-23 15:38:13 +0200

    feat: added a new dashboard page layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,5 @@
+import DashboardBase from "@/components/dashboard/DashboardBase";
+
+export default function DashboardLayout() {
+  return <DashboardBase />;
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,7 @@
-"use client"
 // import LogoutButton from "@/components/login/LogoutButton";
 
-import { DashboardMainPage } from "@/components/dashboard/DashboardMainPage";
-import useSearchBooks from "@/hooks/useSearchBooks";
-
 export default function Dashboard() {
-  const { books } = useSearchBooks();
-  console.log(books);
-  return <DashboardMainPage />;
+  return <></>;
 
   /* <LogoutButton /> */
 }
