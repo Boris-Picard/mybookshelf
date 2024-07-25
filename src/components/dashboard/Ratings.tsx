93ecb068025df213d2dd1bb23d596dcd3eeeb062
boris-picard <picard.boris@gmail.com>
2024-07-25 21:45:51 +0200

    feat(CardList): card almost pdf link in progress

--- src/components/dashboard/Ratings.tsx
+++ src/components/dashboard/Ratings.tsx
@@ -4,16 +4,23 @@ import { Rating, Star } from "@smastrom/react-rating";
 
 import "@smastrom/react-rating/style.css";
 
-
 const myStyles = {
   itemShapes: Star,
-  activeFillColor: '#ffb700',
-  inactiveFillColor: '#fbf1a9'
-}
+  activeFillColor: "#ffb700",
+  inactiveFillColor: "#ddd",
+};
 
 const Ratings: React.FC<Rate> = ({ ratings }: Rate) => {
   return [ratings].map((value) => {
-    return <Rating readOnly value={value} key={value} className="h-6 w-6" itemStyles={myStyles} />;
+    return (
+      <Rating
+        readOnly
+        value={value}
+        key={value}
+        className="h-6 w-6"
+        itemStyles={myStyles}
+      />
+    );
   });
 };
 
--- src/components/dashboard/home/CardList.tsx
+++ src/components/dashboard/home/CardList.tsx
@@ -9,6 +9,8 @@ import { Books } from "@/types/Books";
 import Image from "next/image";
 import Ratings from "@/components/dashboard/Ratings";
 
+import pdf from "/assets/pdf.svg";
+
 interface CardListProps {
   books: Books;
 }
@@ -21,7 +23,7 @@ const CardList: React.FC<CardListProps> = ({ books }) => {
       <div className="flex-shrink-0">
         <Image
           src={books.thumbnail}
-          alt="test"
+          alt={books.title}
           width={180}
           height={180}
           className=" object-cover rounded-tl-xl rounded-bl-xl"
@@ -30,19 +32,31 @@ const CardList: React.FC<CardListProps> = ({ books }) => {
       <div className="flex flex-col flex-grow ml-4 items-center justify-center">
         <CardHeader className="flex flex-row space-y-0 gap-2 items-start">
           <div className="grid gap-1 flex-grow">
-            <CardTitle className="text-lg sm:text-xl font-semibold">
-              {books.title}
-            </CardTitle>
+            <div className="flex justify-between">
+              <CardTitle className="text-lg sm:text-xl font-semibold">
+                <span>{books.title}</span>
+              </CardTitle>
+              <span className="font-semibold">
+                {books.amount} {books.currencyCode}
+              </span>
+            </div>
             <small className="text-muted-foreground">by {books.authors}</small>
             <CardDescription className="text-sm sm:text-base text-pretty line-clamp-2 mt-4">
               {books.description}
             </CardDescription>
-            <div className="flex flex-row">
+            <div className="flex flex-row mt-4">
               <CardContent className="p-0">
                 {books.ratingsCount && (
-                  <p className="text-sm sm:text-base text-muted-foreground">
-                    {books.ratingsCount} ratings <Ratings ratings={books.averageRating} />
-                  </p>
+                  <div className="text-sm sm:text-base text-muted-foreground flex items-center w-full">
+                    <span className="text-muted-foreground w-full">
+                      {books.ratingsCount} ratings
+                    </span>
+                    <Ratings ratings={books.averageRating} />
+                    <span className="text-muted-foreground ml-2">
+                      {books.averageRating}
+                    </span>
+                    <Image src={pdf} width={24} height={24} alt="pdf" />
+                  </div>
                 )}
               </CardContent>
             </div>
