246ccbad126708a55da46d90c4a1d9ef1dc2bcf4
boris-picard <picard.boris@gmail.com>
2024-08-01 12:15:31 +0200

    feat(CategoriesList): added style categories

--- src/components/dashboard/dashboardCategories/CategoriesList.tsx
+++ src/components/dashboard/dashboardCategories/CategoriesList.tsx
@@ -1,19 +1,41 @@
 "use client";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+interface Category {
+  title: string;
+  href: string[];
+  alt: string[];
+  name: string[];
+}
+
+const category: Category[] = [
+  {
+    title: "Art et Design",
+    href: ["/"],
+    alt: ["test"],
+    name: ["art", "Design"],
+  },
+];
 
 const CategoriesList: React.FC = () => {
   return (
     <div className="space-y-3">
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          Make changes to your account here.
-        </TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
-      </Tabs>
+      {category.map((item) => {
+        return (
+          <div className="space-y-3" key={item.name}>
+            <h5 className="font-bold text-xl">{item.title}</h5>
+            <div className="space-x-3">
+              <Button>
+                <Link href={item.href} alt={item.href}>
+                  {item.name}
+                </Link>
+              </Button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
