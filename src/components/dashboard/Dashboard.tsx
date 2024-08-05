5f7aa61019478452abc2792add4dfb4075818020
boris-picard <picard.boris@gmail.com>
2024-08-05 18:02:46 +0200

    feat(Dashboard): change search bar

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -114,12 +114,12 @@ const Dashboard: React.FC<DashboardProps> = ({ user, category, bookId }) => {
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
-          <div className="relative ml-auto flex-1 md:grow-0">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <div className="relative ml-auto flex-1">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground xl:ml-3 xl:w-full" />
             <Input
               type="search"
               placeholder="Search..."
-              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+              className="w-full rounded-lg bg-background pl-8 xl:w-1/2 ml-auto"
             />
           </div>
           <DropdownMenu>
