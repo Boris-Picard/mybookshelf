3d6b7336b14105c5f734736dbb9a6351213720f6
boris-picard <picard.boris@gmail.com>
2024-08-13 01:48:18 +0200

    feat: added features in progress & corrected section padding & marging

--- src/components/home/features/Features.tsx
+++ src/components/home/features/Features.tsx
@@ -1,7 +1,17 @@
 const Features = () => {
   return (
-    <section id="section" className="lg:py-24 relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8 h-screen">
-      <h1 className="text-xl">Features</h1>
+    <section
+      id="section"
+      className="lg:py-24 relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8 h-screen"
+    >
+      <h2 className="mb-2 text-center text-5xl font-bold leading-[1.2] tracking-tighter text-foreground">
+        Features
+      </h2>
+      <h3 className="mx-auto mb-8 text-balance text-center text-lg font-medium tracking-tight text-foreground/80">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quam sed
+        accusantium accusamus eum corrupti minus non itaque, et sapiente aliquam
+        nihil odit ab quidem tempora incidunt, laborum alias veritatis.
+      </h3>
     </section>
   );
 };
