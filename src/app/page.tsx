0854c04c51124cedd548c3929b3667a839186378
boris-picard <picard.boris@gmail.com>
2024-08-12 12:32:06 +0200

    feat(home - Hero): added new folder in components to manage landing page & added hero page in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Navbar from "@/components/Navbar";
+import Hero from "@/components/home/Hero";
+import Navbar from "@/components/home/Navbar";
 import { getCurrentUser } from "@/services/CurrentUser";
 
 export default async function Home() {
   const session = await getCurrentUser();
-  
+
   return (
     <>
       <Navbar id={session?.id} />
-      <main className="flex min-h-screen justify-center items-center p-24"></main>
+      <main className="flex min-h-screen justify-center items-center p-24">
+        <Hero />
+      </main>
     </>
   );
 }
---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section id="hero">
+      <h1 className="text-xl">HERO</h1>
+    </section>
+  );
+};
+
+export default Hero;
