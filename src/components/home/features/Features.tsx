fbe6678ecaa5976538e88d6e3a2098b8d679c1f0
boris-picard <picard.boris@gmail.com>
2024-08-13 12:33:18 +0200

    feat(wobblecards): added all text for each cards

--- src/components/home/features/Features.tsx
+++ src/components/home/features/Features.tsx
@@ -4,17 +4,19 @@ const Features = () => {
   return (
     <section
       id="section"
-      className="lg:py-24 relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8 h-screen"
+      className="lg:py-24 relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8 h-full"
     >
       <h2 className="mb-2 text-center text-5xl font-bold leading-[1.2] tracking-tighter text-foreground">
         Features
       </h2>
       <h3 className="mx-auto mb-8 text-balance text-center text-lg font-medium tracking-tight text-foreground/80">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quam sed
-        accusantium accusamus eum corrupti minus non itaque, et sapiente aliquam
-        nihil odit ab quidem tempora incidunt, laborum alias veritatis.
+        Explorez MyBookshelf, votre plateforme personnelle pour organiser,
+        découvrir et suivre vos lectures favorites. Grâce à une interface
+        intuitive et des fonctionnalités avancées, gérez facilement votre
+        bibliothèque, trouvez des recommandations personnalisées et
+        connectez-vous avec des milliers de livres et auteurs en quelques clics.
       </h3>
-      <div className="xl:py-24 py-12">
+      <div className="py-12">
         <WobleCardsFeatures />
       </div>
     </section>
--- src/components/home/features/WobbleCards.tsx
+++ src/components/home/features/WobbleCards.tsx
@@ -5,38 +5,73 @@ import { WobbleCard } from "@/components/ui/wobble-card";
 
 export function WobleCardsFeatures() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 max-w-7xl mx-auto w-full">
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-4 max-w-7xl mx-auto w-full">
       <WobbleCard
-        containerClassName="col-span-1 h-full bg-pink-800 "
+        containerClassName="col-span-2 h-full bg-primary/40 "
         className=""
       >
         <div className="max-w-xs">
-          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Gippity AI powers the entire universe
+          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
+            Recommandations personnalisées
           </h2>
-          <p className="mt-4 text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+          <p className="mt-4 text-left text-base/6 text-neutral-200">
+            Découvrez de nouveaux livres basés sur vos lectures précédentes et
+            vos favoris. Notre algorithme adapte les suggestions pour toujours
+            correspondre à vos goûts littéraires.
           </p>
         </div>
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 ">
-        <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
+      <WobbleCard containerClassName="col-span-2 bg-primary/50">
+        <h2 className="max-w-80 text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
+          Votre bibliothèque, votre empire
         </h2>
-        <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
+        <p className="mt-4 max-w-[26rem] text-left text-base/6 text-neutral-200">
+          Ajoutez, organisez et suivez tous vos livres en un seul endroit.
+          Marquez les livres comme lus, à lire ou favoris et gardez une trace de
+          votre progrès de lecture.
         </p>
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 bg-blue-900">
+      <WobbleCard containerClassName="col-span-2 bg-primary/60">
         <div className="max-w-sm">
-          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
+          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
+            Authentification facile
           </h2>
           <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+            Connectez-vous en toute simplicité via Google ou GitHub pour accéder
+            à votre bibliothèque personnalisée et partager vos lectures avec la
+            communauté.
+          </p>
+        </div>
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 lg:col-span-4 bg-primary/70">
+        <div className="max-w-sm">
+          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Aperçu de Vos Lectures
+          </h2>
+          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+            Visualisez facilement tous les livres que vous avez lus et ceux en
+            cours dans votre bibliothèque personnelle. Profitez d'un accès
+            rapide à vos favoris et retrouvez vos dernières découvertes en un
+            coup d'œil.
+          </p>
+        </div>
+        <Image
+          src="/assets/favorites.png"
+          width={500}
+          height={500}
+          alt="linear demo image"
+          className="absolute -right-10 md:-right-[40%] lg:-right-[15%] -bottom-5 object-contain rounded-2xl"
+        />
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-2 bg-primary/80">
+        <div className="max-w-sm">
+          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
+            Accès direct à des ressources
+          </h2>
+          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+            Obtenez des liens pour acheter des livres, les télécharger ou les
+            lire en ligne directement depuis Google Books, enrichissant ainsi
+            votre expérience de lecture sans effort.
           </p>
         </div>
       </WobbleCard>
