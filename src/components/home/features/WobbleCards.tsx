5e6cb54ac0c64c1dcbc6fcc5d5fb4b99f4d1e3d1
boris-picard <picard.boris@gmail.com>
2024-08-13 13:33:41 +0200

    feat(WoobleCards): wobble cards done for the moment & features done

--- src/components/home/features/WobbleCards.tsx
+++ src/components/home/features/WobbleCards.tsx
@@ -6,49 +6,48 @@ import { WobbleCard } from "@/components/ui/wobble-card";
 export function WobleCardsFeatures() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6 gap-4 max-w-7xl mx-auto w-full">
-      <WobbleCard
-        containerClassName="col-span-2 h-full bg-primary/40 "
-        className=""
-      >
-        <div className="max-w-xs">
+      <WobbleCard containerClassName="col-span-2 h-full bg-primary/40 ">
+        <div className="sm:max-w-xs min-w-full">
           <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
             Recommandations personnalisées
           </h2>
-          <p className="mt-4 text-left text-base/6 text-neutral-200">
+          <p className="mt-4 text-left text-base/6">
             Découvrez de nouveaux livres basés sur vos lectures précédentes et
             vos favoris. Notre algorithme adapte les suggestions pour toujours
             correspondre à vos goûts littéraires.
           </p>
         </div>
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-2 bg-primary/50">
-        <h2 className="max-w-80 text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
-          Votre bibliothèque, votre empire
-        </h2>
-        <p className="mt-4 max-w-[26rem] text-left text-base/6 text-neutral-200">
-          Ajoutez, organisez et suivez tous vos livres en un seul endroit.
-          Marquez les livres comme lus, à lire ou favoris et gardez une trace de
-          votre progrès de lecture.
-        </p>
-      </WobbleCard>
       <WobbleCard containerClassName="col-span-2 bg-primary/60">
-        <div className="max-w-sm">
+        <div className="sm:max-w-xs min-w-full">
+          <h2 className="max-w-80 text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
+            Votre bibliothèque, votre empire
+          </h2>
+          <p className="mt-4 text-left text-base/6">
+            Ajoutez, organisez et suivez tous vos livres en un seul endroit.
+            Marquez les livres comme lus, à lire ou favoris et gardez une trace
+            de votre progrès de lecture.
+          </p>
+        </div>
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-2 bg-primary/50">
+        <div className="sm:max-w-xs min-w-full">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
             Authentification facile
           </h2>
-          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+          <p className="mt-4 text-left  text-base/6">
             Connectez-vous en toute simplicité via Google ou GitHub pour accéder
             à votre bibliothèque personnalisée et partager vos lectures avec la
             communauté.
           </p>
         </div>
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 lg:col-span-4 bg-primary/70">
-        <div className="max-w-sm">
-          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+      <WobbleCard containerClassName="col-span-1 lg:col-span-4 bg-primary/80">
+        <div className="sm:max-w-xs min-w-full">
+          <h2 className="max-w-sm md:max-w-lg text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
             Aperçu de Vos Lectures
           </h2>
-          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+          <p className="mt-4 xl:max-w-[26rem] text-left text-base/6">
             Visualisez facilement tous les livres que vous avez lus et ceux en
             cours dans votre bibliothèque personnelle. Profitez d'un accès
             rapide à vos favoris et retrouvez vos dernières découvertes en un
@@ -59,16 +58,16 @@ export function WobleCardsFeatures() {
           src="/assets/favorites.png"
           width={500}
           height={500}
-          alt="linear demo image"
-          className="absolute -right-10 md:-right-[40%] lg:-right-[15%] -bottom-5 object-contain rounded-2xl"
+          alt="image des favoris"
+          className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-5 object-contain rounded-2xl hidden xl:block"
         />
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-2 bg-primary/80">
-        <div className="max-w-sm">
+      <WobbleCard containerClassName="col-span-2 bg-primary/60">
+        <div className="sm:max-w-xs min-w-full">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em]">
             Accès direct à des ressources
           </h2>
-          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+          <p className="mt-4 text-left text-base/6">
             Obtenez des liens pour acheter des livres, les télécharger ou les
             lire en ligne directement depuis Google Books, enrichissant ainsi
             votre expérience de lecture sans effort.
