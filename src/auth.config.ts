62053d292db90b3497ac409d326f2717ce20e514
boris-picard <picard.boris@gmail.com>
2024-08-15 12:31:02 +0200

    fix: fixed authorization provider in auth config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,8 +3,26 @@ import Google from "next-auth/providers/google"
 
 import type { NextAuthConfig } from "next-auth"
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+
 export default {
-  providers: [GitHub, Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: isProduction
+        ? "https://mybookshelf-six.vercel.app/api/auth/callback/google"
+        : "http://localhost:3000/api/auth/callback/google"
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      authorization: isProduction
+        ? "https://mybookshelf-six.vercel.app/api/auth/callback/github"
+        : "http://localhost:3000/api/auth/callback/github"
+    }),
+  ],
 } satisfies NextAuthConfig
 
 
