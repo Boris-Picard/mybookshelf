6a05182b64cae66022735bad620eb700a34633a0
boris-picard <picard.boris@gmail.com>
2024-08-15 13:29:59 +0200

    feat: added vercel json config

--- src/components/home/hero/ParticlesBackground.tsx
+++ src/components/home/hero/ParticlesBackground.tsx
@@ -14,7 +14,7 @@ const ParticlesBackground = () => {
   }, [theme]);
 
   return (
-    <div className="absolute inset-0 z-0 h-full w-full">
+    <div className="absolute inset-0 z-10 h-full w-full">
       <Particles
         className="absolute inset-0"
         quantity={100}
