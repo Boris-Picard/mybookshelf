33306321f83272d7e0216465284c57ef9a72a0d9
boris-picard <picard.boris@gmail.com>
2024-08-16 11:55:59 +0200

    feat(env - package): correctly implemented env local & prod & script to migrate bdd if prod or dev mode

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,8 +3,6 @@ import Google from "next-auth/providers/google"
 
 import type { NextAuthConfig } from "next-auth"
 
-const isDevOrProd = process.env.NODE_ENV === "production"
-
 export default {
   providers: [
     Google({
@@ -12,11 +10,11 @@ export default {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     GitHub({
-      clientId: isDevOrProd ? process.env.GITHUB_CLIENT_ID : process.env.GITHUB_LOCAL_CLIENT_ID,
-      clientSecret: isDevOrProd? process.env.GITHUB_CLIENT_SECRET : process.env.GITHUB_LOCAL_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET 
 } satisfies NextAuthConfig
 
 
